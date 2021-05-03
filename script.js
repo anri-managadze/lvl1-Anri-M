387d1c8eaa1a8f5481d289064d196e38b482a302
anri-managadze <anrimanagadze@gmail.com>
2021-05-03 16:14:19 +0400

    commit

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ function searchShow(query) {
     fetch(url)
         .then(resp => resp.json())
         .then((data) => {
-            console.log(data);
+            const result = data.results.map(movie => {
+                return {
+                    artistName: movie.artistName,
+                    trackName:movie.trackName,
+                    trackViewUrl:movie.trackViewUrl
+                }
+            });
+            console.log(result);
+
+            let tmp=`<thead><tr><th>Name</th><th>TrackName</th><th>TrackUrl</th></tr></thead>`;
+            for(let i=0; i< result.length; i++){
+                tmp+=`<tbody><tr><td>${result[i].artistName}</th><td>${result[i].trackName}</td><td><a href="${result[i].trackViewUrl}" target="_blank">Open</a></td></tr></tbody>`
+            }
+            console.log(tmp);
+
+            document.getElementById('output').innerHTML+=tmp;
         })
 }
+
+
 function myFunction() {
     const field = document.getElementById("myForm").querySelector('#text');
     searchShow(field.value);
