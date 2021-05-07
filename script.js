a765dc09e350f4b36580565f00a766201d5ae4f6
anri-managadze <anrimanagadze@gmail.com>
2021-05-07 18:02:29 +0400

    commit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function searchShow(query) {
         .then((data) => {
             const result = data.results.map(movie => {
                 return {
+                    artworkUrl100: movie.artworkUrl100,
                     artistName: movie.artistName,
                     trackName:movie.trackName,
                     previewUrl:movie.previewUrl
@@ -12,18 +13,35 @@ function searchShow(query) {
             });
             console.log(result);
 
-            let tmp=`<thead><tr><th>Name</th><th>TrackName</th><th>TrackUrl</th></tr></thead>`;
+            /*  Table form script
+            let tmp=`<thead><tr><th></th><th>Name</th><th>TrackName</th><th>TrackUrl</th></tr></thead>`;
             for(let i=0; i< result.length; i++){
-                tmp+=`<tbody><tr><td>${result[i].artistName}</th><td>${result[i].trackName}</td><td><audio controls><source src="${result[i].previewUrl}"></audio></td></tr></tbody>`
+                tmp+=`<tbody><tr><td><img src="${result[i].artworkUrl100}" alt="image"></td><td>${result[i].artistName}</td><td>${result[i].trackName}</td><td><audio controls><source src="${result[i].previewUrl}"></audio></td></tr></tbody>`
             }
             console.log(tmp);
+             */
 
+            let tmp=``;
+            for(let i=0; i< result.length; i++) {
+            tmp+= `<div>
+                    <img src="${result[i].artworkUrl100}" alt="image"/>
+                    <h4>${result[i].artistName}</h4>
+                    <h5>${result[i].trackName}</h5>
+                    <audio controls><source src="${result[i].previewUrl}"></audio>
+               </div>`
+            }
             document.getElementById('output').innerHTML+=tmp;
         })
 }
 
-
-function myFunction() {
+document.getElementById('myForm').addEventListener('submit',myFunction);
+function myFunction(e) {
+    e.preventDefault();
     const field = document.getElementById("myForm").querySelector('#text');
     searchShow(field.value);
+}
+document.getElementById('reload').addEventListener('click',myReload);
+function myReload(e) {
+    e.preventDefault();
+    location.reload();
 }
