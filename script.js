c02ce86ba7db63c7980d91f937e1121b560dd779
anri-managadze <anrimanagadze@gmail.com>
2021-05-26 19:04:03 +0400

    ecommerce

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 document.querySelector('#reload').addEventListener('click', function (){location.reload()});
+
+function prodFetch() {
+    const url = `https://fakestoreapi.com/products`;
+    fetch(url)
+        .then(resp => resp.json())
+        .then((data) => {
+            const result = data.map(movie => {
+                return {
+                    image: movie.image,
+                    title:movie.title,
+                    price: movie.price
+                }
+            });
+            console.log(result);
+
+            let tmp=``;
+            for(let i=0; i< result.length; i++) {
+                tmp+= `<div>
+                    <img src="${result[i].image}" alt="image"/>
+                    <div class="star"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i></div>
+                    <h5>${result[i].title}</h5>
+                    <h4>$${result[i].price}</h4>
+               </div>`
+            }
+            document.getElementById('output').innerHTML+=tmp;
+        })
+}
+document.getElementById('many').addEventListener('click',myFunction);
+function myFunction(e) {
+    e.preventDefault();
+    prodFetch();
+}
